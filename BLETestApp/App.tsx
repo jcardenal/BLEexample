3c68056782777e8325af4735da16aa2b545ea29f
jcardenal <jcardenal@sokratia.com>
2020-05-12 18:23:49 +0200

    Call 'NativeEventEmitter' to be able to register callbacks for BLE events

--- BLETestApp/App.tsx
+++ BLETestApp/App.tsx
@@ -1,7 +1,11 @@
 import React, {useEffect} from 'react';
+import {NativeEventEmitter, NativeModules} from 'react-native';
 import { ThemeContext, getTheme, COLOR } from 'react-native-material-ui';
 import MainView from './components/MainView';
 import BleManager from 'react-native-ble-manager';
+
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
 const uiTheme = {
   palette: {
--- BLETestApp/__tests__/App.test.tsx
+++ BLETestApp/__tests__/App.test.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import {render} from 'react-native-testing-library';
 import App from '../App';
-import BleManager from 'react-native-ble-manager';
+import * as BleManager from 'react-native-ble-manager';
+import * as ReactNative from 'react-native';
 
 jest.mock('react-native-ble-manager', () => ({start: jest.fn( () => Promise.resolve(true)) }) );
+jest.mock("react-native", () => {
+    const ReactNative = jest.requireActual('react-native');
 
+    return Object.setPrototypeOf(
+        {
+          NativeModules: {
+            ...ReactNative.NativeModules,
+            BleManager: jest.fn()
+          },
+          NativeEventEmitter: jest.fn(),
+        },
+        ReactNative
+      );
+});
 
 describe("<App />", () => {
 
@@ -16,4 +30,9 @@ describe("<App />", () => {
             render( <App />);
             await expect(BleManager.start).toHaveBeenCalled();
         });
+
+        it("should create 'NativeEventEmitter'", async () => {
+            render( <App />);
+            await expect(ReactNative.NativeEventEmitter).toHaveBeenCalled();
+         });
 })
