60bf123d7a5ac44948194b8a3196446309bc7be5
jcardenal <jcardenal@sokratia.com>
2020-05-13 11:57:54 +0200

    Small refactoring

--- BLETestApp/components/__tests__/ScanningButton.test.tsx
+++ BLETestApp/components/__tests__/ScanningButton.test.tsx
@@ -7,23 +7,27 @@ jest.mock('react-native-ble-manager', () => ({ scan: jest.fn( () => Promise.reso
                                               stopScan: jest.fn( () => Promise.resolve()) }) );
 
 describe("<ScanningButton />", () => {
+
+    let container;
+
+    beforeEach(() => {
+        container = render( <ScanningButton />);
+    });
+
     it("should show 'START SCAN'", async () => {
-        const {getByText} = render( <ScanningButton />);
-        expect(getByText("START SCAN")).toBeTruthy();
+        expect(container.getByText("START SCAN")).toBeTruthy();
     });
     it("should change text to 'STOP SCAN' and back", async () => {
-        const {getByText} = render( <ScanningButton />);
-        fireEvent.press(getByText("START SCAN"));
-        await waitForElement(() => getByText('STOP SCAN'));
-        fireEvent.press(getByText("STOP SCAN"));
-        await waitForElement(() => getByText('START SCAN'));
+        fireEvent.press(container.getByText("START SCAN"));
+        await waitForElement(() => container.getByText('STOP SCAN'));
+        fireEvent.press(container.getByText("STOP SCAN"));
+        await waitForElement(() => container.getByText('START SCAN'));
     });
     it("should call 'scan' on button press and then, 'stopScan'", async () => {
-        const {getByText} = render( <ScanningButton />);
-        fireEvent.press(getByText("START SCAN"));
+        fireEvent.press(container.getByText("START SCAN"));
         await expect(BleManager.scan).toHaveBeenCalledWith([], SCAN_PERIOD_IN_SECONDS, true);
-        await waitForElement(() => getByText('STOP SCAN'));
-        fireEvent.press(getByText("STOP SCAN"));
+        await waitForElement(() => container.getByText('STOP SCAN'));
+        fireEvent.press(container.getByText("STOP SCAN"));
         await expect(BleManager.stopScan).toHaveBeenCalledWith();
     });
 })
