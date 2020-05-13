e5641b61b13d76adbc46bcef1320412e6ef352dd
jcardenal <jcardenal@sokratia.com>
2020-05-13 18:35:44 +0200

    Add new peripherals as they are discovered

--- BLETestApp/components/ServicesList.tsx
+++ BLETestApp/components/ServicesList.tsx
@@ -1,14 +1,31 @@
-import React, {useState} from 'react';
-import {ScrollView} from 'react-native'
+import React, {useState, useEffect, useContext} from 'react';
+import {ScrollView, Text} from 'react-native'
 import BatteryService from './BatteryService';
+import {EmitterContext} from '../App';
 
-const ServicesList = ({list}) => {
+const ServicesList = () => {
+    const emitter = useContext(EmitterContext);
+    const [peripherals, setPeripherals] = useState(new Map());
+
+    useEffect(()=>{
+          emitter.addListener('BleManagerDiscoverPeripheral', (id, name, rssi, advertising) => {
+                    peripheral = { ...{}, id, name, rssi, advertising };
+                    console.log('Discovered peripheral: ', peripheral);
+                    setPeripherals(new Map(peripherals.set(id, peripheral)));
+                 }
+          );
+    }, []);
+
+    const drawPeripherals = () => {
+        return [...peripherals.keys()].map(k => (<BatteryService peripheral={peripherals.get(k)} />));
+    }
 
     return (
-    <ScrollView>
-    {list.map(name => <BatteryService sname={name} />)}
-    </ScrollView>
+        <ScrollView>
+            {drawPeripherals()}
+        </ScrollView>
     )
 }
+
 
 export default ServicesList;
--- BLETestApp/components/__tests__/ServicesList.test.tsx
+++ BLETestApp/components/__tests__/ServicesList.test.tsx
@@ -1,24 +1,71 @@
 import React from 'react';
-import {render} from 'react-native-testing-library';
-import {Text} from 'react-native';
+import {act, render, waitForElement} from 'react-native-testing-library';
 import ServicesList from '../ServicesList';
 import BatteryService from '../BatteryService';
+import {EmitterContext} from '../../App';
 
-jest.mock('../BatteryService', () => jest.fn( () => null) );
+jest.mock('../BatteryService', () => jest.fn( () => null));
+jest.mock("react-native", () => {
+    const ReactNative = jest.requireActual('react-native');
 
+    return Object.setPrototypeOf(
+        {
+          NativeModules: {
+            ...ReactNative.NativeModules,
+            BleManager: jest.fn()
+          },
+          NativeEventEmitter: jest.fn(() => ({addListener: jest.fn() }) ),
+        },
+        ReactNative
+      );
+});
+
+const emitterMock = {addListener: jest.fn()}
 
 describe("<ServicesList />", () => {
-    beforeEach(() => {BatteryService.mockClear()});
+
+    let container;
+
+    beforeEach(() => {
+        BatteryService.mockClear();
+        emitterMock.addListener.mockClear();
+        container = render(
+            <EmitterContext.Provider value={emitterMock}>
+                <ServicesList />
+            </EmitterContext.Provider>
+        );
+    });
 
     it("should render an empty list", () => {
-        render(<ServicesList list={[]} />);
         expect(BatteryService).not.toHaveBeenCalled();
     })
 
-    it("should present two services", () => {
-        render(<ServicesList list={['service1', 'service2']} />);
-        expect(BatteryService).toHaveBeenCalledWith({sname: 'service1'}, {});
-        expect(BatteryService).toHaveBeenCalledWith({sname: 'service2'}, {});
-        expect(BatteryService).toHaveBeenCalledTimes(2);
+    it("should register BLE listener for peripheral discovery", async () => {
+        await expect(emitterMock.addListener)
+                .toHaveBeenCalledWith('BleManagerDiscoverPeripheral', expect.any(Function));
+    });
+
+    it("should add a new peripheral on discovery", async () => {
+        const mockPeripheral = {
+                                 id: '00-11-22',
+                                 name: 'service1',
+                                 rssi: 117,
+                                 advertising: {
+                                    isConnectable: true,
+                                    serviceUUIDs: ['00-11', '11-22'],
+                                    manufacturerData: {},
+                                    serviceData: {},
+                                    txPowerLevel: 23
+                                 }
+                             };
+        act( () =>{ discoverNewService(emitterMock.addListener, mockPeripheral);} );
+        await expect(BatteryService).toHaveBeenCalledWith({peripheral: mockPeripheral}, {});
+        await expect(BatteryService).toHaveBeenCalledTimes(1);
     })
 })
+
+const discoverNewService = (mock, service) => {
+        const lastCall = mock.mock.calls.length -1;
+        const stopScanListener = mock.mock.calls[lastCall][1];
+        stopScanListener(service.id, service.name, service.rssi, service.advertising);
+};
